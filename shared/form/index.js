3f1dc376909d74e3d8f15736c4765d3531c29e48
Guille Paz <guille87paz@gmail.com>
2016-01-26 08:40:11 -0300

    Use virtual-html and create mount and render methods.

--- shared/form/index.js
+++ shared/form/index.js
@@ -1,41 +1,24 @@
 'use strict';
 
-var template = require('./templates/form.js');
-var id = 0;
-
-var VNode = require('virtual-dom/vnode/vnode');
-var VText = require('virtual-dom/vnode/vtext');
-var createElement = require('virtual-dom/create-element');
 var diff = require('virtual-dom/diff');
 var patch = require('virtual-dom/patch');
+var virtualHTML = require('virtual-html');
+var createElement = require('virtual-dom/create-element');
 
+var template = require('./templates/form.js');
 
-var convertHTML = require('html-to-vdom')({
-    VNode: VNode,
-    VText: VText
-});
+var id = 0;
 
 /**
  * Form
  */
 function Form(options) {
+  var self = this;
+
   this.container = options.container;
   this.options = options || {};
   this._id = id++;
-
-  this.virtual = convertHTML(template(this.options));
-  var rootNode = createElement(this.virtual);
-  $(this.container)[0].appendChild(rootNode);
-
-  var self = this;
   this.options.count = 0;
-  setInterval(function () {
-    self.options.count++;
-    var newTree = convertHTML(template(self.options));
-    var patches = diff(self.virtual, newTree);
-    rootNode = patch(rootNode, patches);
-    self.virtual = newTree;
-  }, 1000);
 };
 
 Form.prototype.toHTML = function() {
@@ -45,22 +28,46 @@ Form.prototype.toHTML = function() {
 Form.prototype.UIevents = function() {
   var self = this;
 
+  this.mount();
+
+  setInterval(function() {
+    self.options.count++;
+    self.render();
+  }, 1000);
+
   $(this.container)
 
-    .on('submit.'+this._id, function() {
+    .on('submit.' + this._id, function() {
       alert('foo');
     })
 
-    .on('click.'+this._id, '.js-refresh', function() {
+    .on('click.' + this._id, '.js-refresh', function() {
+      self.options.count = 0;
       self.refresh();
     });
 }
 
+Form.prototype.mount = function() {
+  this.virtual = virtualHTML(template(this.options));
+  this.rootNode = createElement(this.virtual);
+  $(this.container).html(this.rootNode);
+};
+
+Form.prototype.render = function() {
+  var newTree = virtualHTML(template(this.options));
+  var patches = diff(this.virtual, newTree);
+  this.rootNode = patch(this.rootNode, patches);
+  this.virtual = newTree;
+}
+
 Form.prototype.refresh = function() {
-  $(this.container).html(this.toHTML());
+  this.mount();
+  this.render();
 };
 
 Form.prototype.destroy = function() {
+  this.rootNode = undefined;
+  this.virtual = undefined;
   $(this.container)
     .html()
     .off('submit.'+this._id)

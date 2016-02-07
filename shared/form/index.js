7b7d9f854d847b9a33eed43876db08ec27320008
Guille Paz <guille87paz@gmail.com>
2016-02-07 13:18:59 -0300

    Test new diffs

--- shared/form/index.js
+++ shared/form/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var diff = require('virtual-dom/diff');
-var patch = require('virtual-dom/patch');
-var virtualHTML = require('virtual-html');
-var createElement = require('virtual-dom/create-element');
+// var diff = require('virtual-dom/diff');
+// var patch = require('virtual-dom/patch');
+// var virtualHTML = require('virtual-html');
+// var createElement = require('virtual-dom/create-element');
 
 var template = require('./templates/form.js');
+// var diffhtml = require('diffhtml').innerHTML;
+var diffhtml = require('morphdom');
 
 var id = 0;
 
@@ -48,16 +50,19 @@ Form.prototype.UIevents = function() {
 }
 
 Form.prototype.mount = function() {
-  this.virtual = virtualHTML(template(this.options));
-  this.rootNode = createElement(this.virtual);
-  $(this.container).html(this.rootNode);
+  // this.virtual = virtualHTML(template(this.options));
+  // this.rootNode = createElement(this.virtual);
+  // $(this.container).html(this.rootNode);
+  $(this.container).html(template(this.options));
 };
 
 Form.prototype.render = function() {
-  var newTree = virtualHTML(template(this.options));
-  var patches = diff(this.virtual, newTree);
-  this.rootNode = patch(this.rootNode, patches);
-  this.virtual = newTree;
+  // var newTree = virtualHTML(template(this.options));
+  // var patches = diff(this.virtual, newTree);
+  // this.rootNode = patch(this.rootNode, patches);
+  // this.virtual = newTree;
+  // diffhtml(document.querySelector(this.container), template(this.options), { enableWorker: true });
+  diffhtml(document.querySelector(this.container).firstChild, template(this.options));
 }
 
 Form.prototype.refresh = function() {
